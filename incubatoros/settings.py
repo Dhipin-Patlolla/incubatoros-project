1c900e606aa2bc34d97776c65992ed9b9dccf5e1
Dhipin-Patlolla <patlolla.dhipin@students.iiserpune.ac.in>
2025-07-28 13:35:06 +0530

    Fix Redis configuration for Render

--- incubatoros/settings.py
+++ incubatoros/settings.py
@@ -122,8 +122,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
